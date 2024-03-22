d4a9e03b873cdb9cd26708f990194836a25b9c24
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2024-03-22 17:11:11 -0300

    Criando config basica para o rabbitMQ

---
+++ infrastructure/rabbitMQ/rabbitMQ.go
@@ -0,0 +1,15 @@
+package rabbitMQ
+
+import (
+	"log"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+func ConectarRabbitMQ() *amqp091.Connection {
+	conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatalf("Falha ao conectar com RabbitMQ: %s", err)
+	}
+	return conn
+}

ff00a98785d58795f375c6e32b41006b34b1c5da
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2024-03-22 17:12:32 -0300

    Add sender para fila de registro de ponto Criando service para lidar com o a logica de envio para fila

---
+++ queue/pontoSender.go
@@ -0,0 +1,71 @@
+package queue
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"pontomenos-api/models"
+
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type PontoSender struct {
+    rabbitMQConnection *amqp091.Connection
+}
+
+func NewPontoSender(rabbitMQConnection *amqp091.Connection) *PontoSender {
+    return &PontoSender{rabbitMQConnection: rabbitMQConnection}
+}
+
+func (ps *PontoSender) EnviaRegistroParaFila(login string) error {
+    ch, err := ps.rabbitMQConnection.Channel()
+    if err != nil {
+        return err
+    }
+    defer ch.Close()
+
+    // Declara a fila como durável
+    queue, err := ch.QueueDeclare(
+        "registroPontoEvento", // nome da fila
+        true,                  // durável
+        false,                 // delete quando não utilizada
+        false,                 // exclusiva
+        false,                 // no-wait
+        nil,                   // argumentos
+    )
+    if err != nil {
+        return err
+    }
+
+    registro := models.RegistroPontoEvento{
+        DataHora: time.Now(),
+        Login:    login,
+    }
+
+    body, err := json.Marshal(registro)
+    if err != nil {
+        return err
+    }
+
+    // Publica a mensagem com contexto e marca como persistente
+    err = ch.PublishWithContext(
+        context.Background(), // contexto
+        "",                  // exchange
+        queue.Name,          // routing key (nome da fila)
+        false,               // mandatory
+        false,               // immediate
+        amqp091.Publishing{
+            ContentType:  "application/json",
+            Body:         body,
+            DeliveryMode: amqp091.Persistent, // mensagem persistente
+        },
+    )
+    if err != nil {
+        return err
+    }
+
+    log.Printf(" [x] Sent %s", body)
+    return nil
+}
---
+++ services/registroPontoEventoService.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"log"
+	"pontomenos-api/queue"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type RegistroPontoEventoService struct {
+	pontoSender *queue.PontoSender
+}
+
+func NewRegistroPontoEventoService(pontoSender *queue.PontoSender) *RegistroPontoEventoService {
+	return &RegistroPontoEventoService{
+		pontoSender: pontoSender,
+	}
+}
+
+func (rpes *RegistroPontoEventoService) RegistraPonto(c *gin.Context) error {
+	// não sei fazer try_catch ne nessa linguagem, to ficando maluco bixo (╯ ͠° ͟ʖ ͡°)╯┻━┻
+	log.Println("Token Recebido na service:", c.GetHeader("Authorization"))
+	tokenString := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.MapClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("pontomenos-fiap"), nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if claims, ok := token.Claims.(*jwt.MapClaims); ok && token.Valid {
+		login, ok := (*claims)["sub"].(string)
+		if !ok {
+			return err 
+		}
+
+		return rpes.pontoSender.EnviaRegistroParaFila(login)
+	}
+
+	return err 
+}
